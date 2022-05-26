fa1dae880208f5a973890b6c9335c997f50087af
Surabhi <surabhi.mahawar@aurigait.com>
2022-05-26 17:12:33 +0530

    changes for docker - uci pwa, uci admin

--- install.sh
+++ install.sh
@@ -12,6 +12,27 @@ cd odk-aggregate
 git clone -b release-4.4.0  https://github.com/samagra-comms/odk.git
 cd ..
 git clone -b release-4.7.0 https://github.com/samagra-comms/uci-apis.git
+
+# UCI Web Channel
+git clone https://github.com/samagra-comms/uci-web-channel.git
+cd uci-web-channel
+# uciWebChannelBaseURL="export const socket = io('ws://localhost:3005/');"
+# sed -i "7s|^.*$|$uciWebChannelBaseURL|" src/websocket.ts
+yarn install
+yarn build
+cd ..
+
+
+# UCI Admin
+git clone https://github.com/samagra-comms/uci-admin
+cd uci-admin
+uciAdminBaseURL="url: 'http://localhost:9999',"
+sed -i "3s|^.*$|$uciAdminBaseURL|" src/environments/environment.prod.ts
+npm install -g @angular/cli
+npm i
+ng build --prod
+cd ..
+
 docker-compose up -d fa-search fusionauth fa-db
 sleep 60s
 docker-compose up -d cass kafka schema-registry zookeeper connect akhq
