fb9fd93ea290ce4c8acfd27e5c8f3edc34605a34
Tushar <codingid6@gmail.com>
2023-04-17 12:28:11 +0000

    Install npm, node and build path for UCI-API

--- setup-manual.sh
+++ setup-manual.sh
@@ -12,6 +12,51 @@ done
 echo ""
 }
 
+
+# Define versions
+INSTALL_NODE_VER=16.14.0
+INSTALL_NVM_VER=0.39.1
+INSTALL_YARN_VER=1.22.17
+
+echo "==> Ensuring .bashrc exists and is writable"
+touch ~/.bashrc
+
+echo "==> Installing node version manager (NVM). Version $INSTALL_NVM_VER"
+# Removed if already installed
+rm -rf ~/.nvm
+# Unset exported variable
+export NVM_DIR=
+
+# Install nvm 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v$INSTALL_NVM_VER/install.sh | bash
+# Make nvm command available to terminal
+source ~/.nvm/nvm.sh
+
+echo "==> Installing node js version $INSTALL_NODE_VER"
+nvm install $INSTALL_NODE_VER
+
+echo "==> Make this version system default"
+nvm alias default $INSTALL_NODE_VER
+nvm use default
+
+#echo -e "==> Update npm to latest version, if this stuck then terminate (CTRL+C) the execution"
+#npm install -g npm
+
+echo "==> Installing Yarn package manager"
+rm -rf ~/.yarn
+curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version $INSTALL_YARN_VER
+
+echo "==> Adding Yarn to environment path"
+# Yarn configurations
+export PATH="$HOME/.yarn/bin:$PATH"
+yarn config set prefix ~/.yarn -g
+
+echo "==> Checking for versions"
+nvm --version
+node --version
+npm --version
+yarn --version
+
 echo "Welcome to the installation script for UCI"
 echo ""
 echo "Let's first start by exporting keys required for installation"
